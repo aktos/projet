24330dc33601001d7e358f0098ae18c9038a885f
aktos <yannou97232@hotmail.com>
2012-12-20 18:40:01 +0100

    requete modif

--- projet/src/main/java/console/Requete.java
+++ projet/src/main/java/console/Requete.java
@@ -3,6 +3,7 @@ package console;
 import java.util.Scanner;
 
 public class Requete {
+	private String requete;
 
 	public Requete(){}
 	
@@ -11,23 +12,26 @@ public class Requete {
 		System.out.println(phrase);
 		long entier = sc.nextLong();
 		sc.close();
-		return (int) entier;
+		int entierF=(int)entier;
+		return  entierF;
 	}
 	
 	public String demanderString(String phrase){
 		Scanner sc = new Scanner(System.in);
 		System.out.println(phrase);
-		String str = sc.next().toString();
+		String str = sc.nextLine();
 		sc.close();
 		return str;
 	}
 	public char demanderChar(String phrase){
 		Scanner sc = new Scanner(System.in);
 		System.out.println(phrase);
-		String str = sc.next().toString();
+		String str = sc.nextLine();
 		char strR=str.charAt(0);
 		sc.close();
 		return strR;
 	}
+	
+	
 
 }
